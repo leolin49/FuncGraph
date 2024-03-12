03975947fc12055c3a4fbc469437be184e3eaede
leolin49 <leolin49@foxmail.com>
2024-03-12 16:45:49 +0800

    添加 readme.md 使用说明

--- func.py
+++ func.py
@@ -1,3 +1,10 @@
+# Copyright 2024 The FCAV Authors. All rights reserved.
+# Use of this source code is governed by a Apache2.0-style
+# license that can be found in the LICENSE file.
+#
+# Time    : 2024/3/12 16:40
+# Author  : linyf49@qq.com
+# File    : func.py
 class Func:
     def __init__(self, fid: int, name: str, param: str, ret: str, lineno: int):
         self.id = fid
--- func_go.py
+++ func_go.py
@@ -1,3 +1,11 @@
+# Copyright 2024 The FCAV Authors. All rights reserved.
+# Use of this source code is governed by a Apache2.0-style
+# license that can be found in the LICENSE file.
+#
+# Time    : 2024/3/12 16:40
+# Author  : linyf49@qq.com
+# File    : func_go.py
+
 import re
 
 import util
